e474b47b7311c12098541079155d7d8cc6bd9690
Christopher Lutz <chrisblutz@users.noreply.github.com>
2016-12-02 08:48:12 -0500

    Add clean-up phase to scraper.sh

--- scraper.sh
+++ scraper.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bas
+#!/usr/bin/env bash
 echo "Updating dependencies..."
 bundle install                  # Make sure Ruby gems (specifically jsontodb) are installed
 bundle update
@@ -9,5 +9,8 @@ java -jar bin/scraper.jar       # Scrape the SACS webpage
 echo "Scraped!  Uploading..."
 jsontodb jsontodb-config.yml    # Post the scraped data
 
-echo "Finished!"
+echo "Uploaded!  Cleaning up..."
+rm -rf json/                    # Remove json/ output directory and generated config file
+rm jsontodb-config.yml
 
+echo "Finished!"
