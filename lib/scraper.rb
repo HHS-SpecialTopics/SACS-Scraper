4a08a81cdd50afebcb583c1133baa3fadc20ea12
Christopher Lutz <lutzblox@gmail.com>
2017-02-21 10:46:29 -0500

    Fix caching

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,11 +16,11 @@ links.each do |link|
   title = subpage.css('div.ui-widget-header h1 span')[0].text.strip
   text = subpage.css('div.ui-widget-detail')[0].to_s
                 .gsub("\r\n", ' ')
-                .gsub('src="/', 'src="http://www.sacs.k12.in.us/')
+                .gsub('src="/', 'src="http://www.sacs.k12.in.us/').strip
                 .gsub('href="/', 'href="http://www.sacs.k12.in.us/')
 
   json = { title: title, content: text,
-           status: 'publish', 'categories[]' => [7], 'tags[]' => [6,8] }
+           status: 'publish', 'categories[]' => [2], 'tags[]' => [5,6] }
   json_obj = JSON.pretty_generate json
 
   File.open("json/#{index}.json", 'w') do |file|
@@ -33,11 +33,12 @@ links.each do |link|
 end
 
 cache = 0
-cache = File.read('config/cache.dat') if File.exist?('config/cache.dat')
+cache = File.read('config/cache.dat').to_i if File.exist?('config/cache.dat')
 
+info_hash = SafeYAML.load(File.read('config/info.yml'), 'config/info.yml')
+result_yaml = { 'User' => info_hash['User'], 'Pass' => info_hash['Pass'] }
+  
 if (index - cache) > 0
-  info_hash = SafeYAML.load(File.read('config/info.yml'), 'config/info.yml')
-  result_yaml = { 'User' => info_hash['User'], 'Pass' => info_hash['Pass'] }
   url = info_hash['Url']
   matrix = []
 
@@ -47,12 +48,12 @@ if (index - cache) > 0
   end
 
   result_yaml['Matrix'] = matrix
+end
 
-  File.open('jsontodb-config.yml', 'w') do |file|
-    file.write(result_yaml.to_yaml)
-  end
+File.open('jsontodb-config.yml', 'w') do |file|
+  file.write(result_yaml.to_yaml)
 end
 
 File.open('config/cache.dat', 'w') do |file|
-  file.write(cache)
+  file.write(index)
 end
